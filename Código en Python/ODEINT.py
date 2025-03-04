5d947847a5a0740def6ffb225b66836ab5768e90
lelopezm <eduardo.lopez@ucaldas.edu.co>
2025-03-04 16:06:31 -0500

    Códigos en python

---
+++ Código en Python/ODEINT.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+#definimos f(t,x) de x'=f(t,x)
+def f(x,t):
+    return -t/x
+
+#tiempo final y número de iteraciones 
+tf=1
+n=200
+#condiciones iniciales
+t0=0
+x0=1
+
+#Solución aproximada 
+T=np.linspace(t0,tf,n)
+Sol = odeint(f,x0,T) 
+X=Sol[:,0]
+
+#gráfica de la solución
+plt.figure(figsize=(10, 6))
+plt.plot(T,X,'#12CFE1')
+plt.grid()
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.title('Solución de \dot{x}=-t/x, x(0)=1')
+plt.show()
+
+#RESULTADOS NUMÉRICOS
+h=(tf-t0)/n #tamaño de paso
+ve=0 #valor exacto de x(tf)
+X_aprox=X[-1] #valor aproximado con odeint
+E=abs(X_aprox-ve)
+print('Valor exacto: x(%d)=%.6f'%(tf,ve))
+print('Valor aproximado: x(%d)=%1.6f'%(tf,X_aprox))
+print('Error estimado: E=%1.12f con un tamaño de paso h=%1.3f'%(E,h))
---
+++ Código en Python/Script_PVI.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+#Función f(t,x)
+def f(t,x):
+    return -x + np.sin(t)
+
+#tiempo
+t0=0
+tf=2*np.pi
+
+#condición inicial
+x0=1
+
+#cantidad de puntos a graficas
+n=200
+
+#Solución aproximada 
+T=np.linspace(t0,tf,n)
+Sol = odeint(f,x0,T) 
+X=Sol[:,0]
+
+#gráfica de la solución
+plt.plot(T,X,'#aa1e0e')
+plt.grid()
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.title('Solución con ODEINT')
+plt.show()
